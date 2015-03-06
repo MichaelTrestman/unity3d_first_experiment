b1d1aa8694efcb36b839f6316276996e32662247
Michael Trestman <michael.a.trestman@gmail.com>
2015-03-06 15:07:19 -0800

    initial commit

---
+++ Assets/MouseLookScript.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var lookSensitivity : float = 5.0;
+var lookSmoothDamp : float = 0.1;
+
+@HideInInspector
+var xRotation : float;
+@HideInInspector
+var yRotation : float;
+@HideInInspector
+var  currentXRotation : float;
+@HideInInspector
+var currentYRotation : float;
+@HideInInspector
+var xRotationV : float;
+@HideInInspector
+var yRotationV : float;
+
+    
+function Update () {
+  yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+  xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+  xRotation = Mathf.Clamp(xRotation, -80, 80);
+  currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, xRotationV, lookSmoothDamp);
+  currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, yRotationV, lookSmoothDamp);
+  
+  transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+}
---
+++ Assets/PlayerMovementScript.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var walkAcceleration : float = 5;
+var cameraObject : GameObject;
+
+function Update () {
+
+	transform.rotation = Quaternion.Euler(0, cameraObject.GetComponent(MouseLookScript).currentYRotation, 0);
+	rigidbody.AddRelativeForce(Input.GetAxis("Horizontal")*walkAcceleration, 0, Input.GetAxis("Vertical")*walkAcceleration);
+}
